986d7b1fea7a0ef3421c32ecc3e4b4d52e73df0e
FranciscoLi2000 <francisco2000li@icloud.com>
2025-01-15 11:19:14 +0000

    K&R

--- temperature.c
+++ temperature.c
@@ -8,6 +8,7 @@ int main(void)
     upper = 300;    // upper limit
     step = 20;      // step size
 
+    printf("The Conversion between Celsius and Fahrenheit:\n");
     fahr = lower;
     while (fahr <= upper) {
         celsius = 5 * (fahr - 32) / 9;
